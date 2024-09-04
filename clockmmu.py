1374df82bf97d7c7a9e048966d612e2c688507b1
Savannah <savannahfung@hotmail.com>
2024-09-04 17:35:50 +0930

    modified replacement_mode in memsim.py

--- clockmmu.py
+++ clockmmu.py
@@ -3,57 +3,76 @@ from mmu import MMU
 
 class ClockMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for EscMMU
-        # pass
-        self.frames = frames
-        self.memory = {}
-        self.page_table = []
-        self.pointer = 0
-        self.disk_reads = 0
-        self.disk_writes = 0
-        self.page_faults = 0
-        self.debug = False
+        self.frames = frames    # Number of frames in memory
+        self.memory = {}        # Memory dictionary to store pages
+        self.page_table = []    # Page table to keep track of pages in memory
+        self.pointer = 0        # Pointer to keep track of the page to replace
+        self.disk_reads = 0     # Number of disk reads
+        self.disk_writes = 0    # Number of disk writes
+        self.page_faults = 0    # Number of page faults
+        self.debug = False      # Debug flag
 
     def set_debug(self):
-        # TODO: Implement the method to set debug mode
-        # pass
         self.debug = True
 
     def reset_debug(self):
-        # TODO: Implement the method to reset debug mode
-        # pass
         self.debug = False
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        # pass
-        if page_number in self.memory:
-            self.memory[page_number][1] = True
+        if self.debug:
+            print(f"Page number: {page_number}")
+            print(f"Mode: R")
+            print()
+
+        if page_number in self.memory:  # Page is in memory 
+            self.memory[page_number][1] = True 
             if self.debug:
                 print(f"Page {page_number} read from memory.")
-        else:
+        else:   # Page is not in memory
             self.page_faults += 1
             self.replace_page(page_number, 'R')
+        
+        if self.debug:
+            print(f"Memory: {self.memory}")
+            print(f"Page table: {self.page_table}")
+            print(f"Pointer: {self.pointer}")
+            print(f"Disk reads: {self.disk_reads}")
+            print(f"Disk writes: {self.disk_writes}")
+            print(f"Page faults: {self.page_faults}")
+            print()
 
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        # pass
-        if page_number in self.memory:
+        if self.debug:
+            print(f"Page number: {page_number}")
+            print(f"Mode: W")
+            print()
+
+        if page_number in self.memory:  # Page is in memory
             self.memory[page_number][0] = 'W'
-            self.memory[page_number][1] = True
+            self.memory[page_number][1] = True 
             if self.debug:
                 print(f"Page {page_number} written to memory.")
-        else:
+        else:   # Page is not in memory
             self.page_faults += 1
             self.replace_page(page_number, 'W')
+
+        if self.debug:
+            print(f"Memory: {self.memory}")
+            print(f"Page table: {self.page_table}")
+            print(f"Pointer: {self.pointer}")
+            print(f"Disk reads: {self.disk_reads}")
+            print(f"Disk writes: {self.disk_writes}")
+            print(f"Page faults: {self.page_faults}")
+            print()
+
     
     def replace_page(self, page_number, mode):
-        if len(self.page_table) < self.frames:
+        if len(self.page_table) < self.frames:  # Memory is not full 
             self.page_table.append(page_number)
             self.memory[page_number] = [mode, True]  # [status, reference bit]
             if self.debug:
                 print(f"Page {page_number} loaded into memory.")
-        else:
+        else:   # Memory is full 
             while True:
                 current_page = self.page_table[self.pointer]
                 if not self.memory[current_page][1]:  # Reference bit is 0
@@ -61,28 +80,22 @@ class ClockMMU(MMU):
                         self.disk_writes += 1
                         if self.debug:
                             print(f"Page {current_page} written back to disk.")
-                    del self.memory[current_page]
-                    self.page_table[self.pointer] = page_number
+                    del self.memory[current_page]   # Remove the page from memory
+                    self.page_table[self.pointer] = page_number # Replace the page in the page table
                     self.memory[page_number] = [mode, True]  # [status, reference bit]
                     if self.debug:
                         print(f"Page {current_page} replaced by page {page_number}.")
                     break
                 self.memory[current_page][1] = False  # Reset the reference bit to 0
-                self.pointer = (self.pointer + 1) % len(self.page_table)  # Circular increment
+                self.pointer = (self.pointer + 1) % len(self.page_table) # Move the pointer to the next page
 
-        self.disk_reads += 1
+        self.disk_reads += 1 # Read from disk
 
     def get_total_disk_reads(self):
-        # TODO: Implement the method to get total disk reads
-        # return -1
         return self.disk_reads
 
     def get_total_disk_writes(self):
-        # TODO: Implement the method to get total disk writes
-        # return -1
         return self.disk_writes
 
     def get_total_page_faults(self):
-        # TODO: Implement the method to get total page faults
-        # return -1
         return self.page_faults
--- randmmu.py
+++ randmmu.py
@@ -3,29 +3,21 @@ import random
 
 class RandMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for RandMMU
-        # pass
-        self.frames = frames
-        self.memory = {}
-        self.page_table = []
-        self.disk_reads = 0
-        self.disk_writes = 0
-        self.page_faults = 0
-        self.debug = False
+        self.frames = frames    # Number of frames in memory
+        self.memory = {}        # Memory dictionary to store pages
+        self.page_table = []    # Page table to keep track of pages in memory
+        self.disk_reads = 0     # Number of disk reads 
+        self.disk_writes = 0    # Number of disk writes
+        self.page_faults = 0    # Number of page faults
+        self.debug = False      # Debug flag
 
     def set_debug(self):
-        # TODO: Implement the method to set debug mode
-        # pass
         self.debug = True
 
     def reset_debug(self):
-        # TODO: Implement the method to reset debug mode
-        # pass
         self.debug = False
 
     def read_memory(self, page_number):
-        # TODO: Implement the method to read memory
-        # pass
         if page_number in self.memory:
             if self.debug:
                 print(f"Page {page_number} read from memory.")
@@ -43,8 +35,6 @@ class RandMMU(MMU):
             self.disk_reads += 1
 
     def write_memory(self, page_number):
-        # TODO: Implement the method to write memory
-        # pass
         if page_number in self.memory:
             self.memory[page_number] = 'W'
             if self.debug:
@@ -65,16 +55,10 @@ class RandMMU(MMU):
             self.disk_reads += 1
 
     def get_total_disk_reads(self):
-        # TODO: Implement the method to get total disk reads
-        # return -1
         return self.disk_reads
 
     def get_total_disk_writes(self):
-        # TODO: Implement the method to get total disk writes
-        # return -1
         return self.disk_writes
 
     def get_total_page_faults(self):
-        # TODO: Implement the method to get total page faults
-        # return -1
         return self.page_faults
